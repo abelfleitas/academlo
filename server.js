44f265bb66cde6970a96115255a6a941eba4cfd6
abel.fleitas <abel.fleitas@ntsprint.com>
2022-06-12 22:32:28 -0400

    Fixed variable

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/public/'));
 
 const PORT = process.env.PORT || 80;
-server.listen(PORT, () => {
-  console.log(`Servidor web escuchando en el puerto${PORT}...`);
+app.listen(PORT, () => {
+  console.log(`Servidor web escuchando en el puerto ${PORT}...`);
 });
