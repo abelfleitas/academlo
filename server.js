04c0a545de6d8ca216d9ba8d16ca52e1e7a3bf52
abel.fleitas <abel.fleitas@ntsprint.com>
2022-06-12 22:29:10 -0400

    change port settings for the server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/public/'));
 
-app.listen('3000', function() {
-  console.log('Servidor web escuchando en el puerto 3000');
+const PORT = process.env.PORT || 80;
+server.listen(PORT, () => {
+  console.log(`Servidor web escuchando en el puerto${PORT}...`);
 });
