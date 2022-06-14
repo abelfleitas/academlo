e3c8a4a06cfe97b17a91afbcbaeec0975c6e7759
abel.fleitas <abel.fleitas@ntsprint.com>
2022-06-14 15:58:24 -0400

    Add task to the view

--- public/assets/dashboard.js
+++ public/assets/dashboard.js
@@ -1,11 +1,34 @@
-(function() {
+( async function() {
 
     if(localStorage.getItem("user") === null){
         window.location = "../index.html";
     }
 
-    var taskArray = list();
-    const task = document.querySelector(".tasks");
+   
+
+    const res = await list();
+    const tasks = document.querySelector(".tasks");
+    res.forEach(element => {
+        
+        let card = document.createElement("div");
+        card.setAttribute("class", "card")
+        
+        let cardHeader = document.createElement("div");
+        cardHeader.setAttribute("class", "card-header");
+        
+        let cardhading = document.createElement("h2");
+        cardhading.innerHTML = element.name;
+
+        let hr = document.createElement("hr");
+
+        cardHeader.append(cardhading);
+        cardHeader.append(hr)
+        card.append(cardHeader);
+
+        //cardhading.setAttribute("text")
+
+        tasks.append(card);
+    });
 })();
 
 
