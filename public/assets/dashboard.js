ef8ef840885f1b2c40804d0dad2407591a99c0c3
abel.fleitas <abel.fleitas@ntsprint.com>
2022-06-14 17:03:03 -0400

    Add items to compenent task

--- public/assets/dashboard.js
+++ public/assets/dashboard.js
@@ -17,18 +17,45 @@
         cardHeader.setAttribute("class", "card-header");
         
         let cardhading = document.createElement("h2");
-        cardhading.innerHTML = element.name;
+        cardhading.innerText = element.name;
+
+        let cardEditBtn = document.createElement("button");
+        cardEditBtn.setAttribute("class","card-edit-btn");
+        cardEditBtn.setAttribute("id",`btn-${element.id}`);
+        cardEditBtn.setAttribute("value", element.id)
 
         let hr = document.createElement("hr");
 
         cardHeader.append(cardhading);
+        cardHeader.append(cardEditBtn);
         cardHeader.append(hr)
-        card.append(cardHeader);
 
-        //cardhading.setAttribute("text")
+        let cardBody = document.createElement("div");
+        cardBody.setAttribute("class","card-body");
+
+        let cardBodyTitle = document.createElement("p");
+        cardBodyTitle.setAttribute("class","card-body-title");
+        cardBodyTitle.innerText = "Descripción";
+
+        let cardBodyMessage = document.createElement("p");
+        cardBodyMessage.setAttribute("class","card-body-message");
+        cardBodyMessage.innerText = element.description;
+
+        cardBody.append(cardBodyTitle);
+        cardBody.append(cardBodyMessage);
+       
+
+        card.append(cardHeader);
+        card.append(cardBody)
 
         tasks.append(card);
     });
+
+    const btn = document.querySelector(".card-edit-btn");
+    btn.addEventListener('click', () => {
+        alert("click => ");
+    });
+
 })();
 
 
