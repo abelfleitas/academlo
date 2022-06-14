588c4c09b9e91ce7c0a72dac0e09c241292c1a01
Abel Alejandro <afleitasp@gmail.com>
2022-06-14 02:36:15 -0400

    Add axios services

---
+++ public/assets/api-rest.js
@@ -0,0 +1,42 @@
+const APIURL = "https://tasks-crud.academlo.com/api";
+
+const button = document.querySelector(".button");
+
+const login = (email,password)  => {
+    axios.post(`${APIURL}/auth/login`, {email, password})
+      .then(function (response) {
+        saveStorage(response.data)
+      })
+      .catch(function (error) {
+        showAlert(error.message);
+        button.classList.remove("button--loading");
+      });
+}
+
+const getUser = () => {
+    const token = localStorage.getItem('token');
+    axios.get(`${APIURL}/user`, { headers: {"Authorization" : `Bearer ${token}`} })
+    .then(function (response) {
+        button.classList.remove("button--loading");
+        saveUserStorage(response.data)
+      })
+      .catch(function (error) {
+        showAlert(error.message);
+        button.classList.remove("button--loading");
+      });
+}
+
+const saveStorage = (response) => {
+    localStorage.setItem("token", response);
+    getUser();
+}
+
+const saveUserStorage = (response) => {
+    localStorage.setItem("user", response);
+    window.location = "../pages/dashboard.html"
+}
+
+const showAlert = (msj) => {
+    alert(msj);
+}
+
