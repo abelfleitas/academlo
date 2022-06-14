15216a3b7c2d45b473c46ca45dce9a148449fb3f
Abel Alejandro <afleitasp@gmail.com>
2022-06-14 01:56:56 -0400

    Update form loing style and install Axios

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/public/'));
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor web escuchando en el puerto ${PORT}...`);
 });
